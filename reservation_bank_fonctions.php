bcf76e6fa489fcdff8730ecb04aceab134ad089f
Rainer <rainer@websimple.be>
2015-09-26 10:42:36 +0200

    importation depuis fabrique

---
+++ reservation_bank_fonctions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fonctions utiles au plugin Réseŕvations Bank
+ *
+ * @plugin     Réseŕvations Bank
+ * @copyright  2015
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Reservation_bank\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/*
+ * Un fichier de fonctions permet de définir des éléments
+ * systématiquement chargés lors du calcul des squelettes.
+ *
+ * Il peut par exemple définir des filtres, critères, balises, …
+ * 
+ */
+
+?>
---
+++ reservation_bank_pipelines.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Utilisations de pipelines par Réseŕvations Bank
+ *
+ * @plugin     Réseŕvations Bank
+ * @copyright  2015
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Reservation_bank\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
+
+
+
+
+?>
