b6dd0eb9fb4940ef0ed6ee887f7dca2c571be9f3
Rainer <rainer@websimple.be>
2015-09-26 10:50:49 +0200

    orthographe

--- lang/paquet-reservation_bank_fr.php
+++ lang/paquet-reservation_bank_fr.php
@@ -8,8 +8,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// R
 	'reservation_bank_description' => 'Intègre la solution de paiement Bank (https://github.com/nursit/bank) dans Réservation Événements
 ',
-	'reservation_bank_nom' => 'Résérvations Bank',
-	'reservation_bank_slogan' => 'Payezvos réservations avec Bank!',
+	'reservation_bank_nom' => 'Réservations Bank',
+	'reservation_bank_slogan' => 'Payez vos réservations avec Bank!',
 );
 
 ?>
