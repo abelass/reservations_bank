0a49e5570a5cf03be5c58c89fa12b067f45a6da6
Rainer <rainer@websimple.be>
2015-09-26 17:34:46 +0200

    format

--- reservation_bank_fonctions.php
+++ reservation_bank_fonctions.php
@@ -9,15 +9,14 @@
  * @package    SPIP\Reservation_bank\Fonctions
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
+if (!defined('_ECRIRE_INC_VERSION'))
+  return;
 
 /*
  * Un fichier de fonctions permet de définir des éléments
  * systématiquement chargés lors du calcul des squelettes.
  *
  * Il peut par exemple définir des filtres, critères, balises, …
- * 
+ *
  */
-
 ?>
