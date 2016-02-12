6093ba8ef240278a5cbad019fb7594ff9a06f401
Rainer Müller <rainer@websimple.be>
2016-02-12 09:21:07 +0100

    possibilité de cacher le formulaire public de paiement

--- lang/reservation_bank_fr.php
+++ lang/reservation_bank_fr.php
@@ -5,8 +5,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// L
+	'label_cacher_paiement_public' => 'ne pas proposer de formulaire de paiement sur le site public',
+
 	// R
 	'reservation_bank_titre' => 'Réservations Bank',
+
+	// T
+	'titre_page_configurer_reservation_bank' => 'Configuration Réservations Bank'
 );
 
 ?>
--- reservation_bank_pipelines.php
+++ reservation_bank_pipelines.php
@@ -10,7 +10,7 @@
  */
 
 if (!defined('_ECRIRE_INC_VERSION'))
-  return;
+	return;
 
 /**
  * Intervient au traitement du formulaire
@@ -19,12 +19,14 @@ if (!defined('_ECRIRE_INC_VERSION'))
  * @param  array $flux Données du pipeline
  * @return array       Données du pipeline
  */
-  function reservation_bank_formulaire_traiter($flux){
-    $form = $flux['args']['form'];
-    // si creation d'un nouvel article lui attribuer la licence par defaut de la config
-    if ($form == 'reservation') {
-      $flux['data']['message_ok'] .= recuperer_fond('inclure/paiement',
-        array('id_reservation' => session_get('id_reservation')));
-    }
-    return $flux;
-  }
+function reservation_bank_formulaire_traiter($flux) {
+	$form = $flux['args']['form'];
+	// Affiche le formulaire de paiment au retour du formulaire réservation
+	if ($form == 'reservation') {
+		include_spip('inc/config');
+		if (!$cacher_paiement_public = lire_config('reservation_bank/cacher_paiement_public')) {
+			$flux['data']['message_ok'] .= recuperer_fond('inclure/paiement', array('id_reservation' => session_get('id_reservation')));
+		}
+	}
+	return $flux;
+}
