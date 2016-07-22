fdc2066a157e9842012826b746c4b6983c3c3930
abelass <rainer@websimple.be>
2016-07-22 14:29:13 +0200

    enregistrement de commande

--- lang/reservation_bank_fr.php
+++ lang/reservation_bank_fr.php
@@ -8,19 +8,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// L
 	'label_cacher_paiement_public' => 'ne pas proposer de formulaire de paiement sur le site public',
-	'label_fieldset_montant_detail' => 'Spécifiez le montant (en @devise@) pour chaque détail de réservation',	
+	'label_fieldset_montant_detail' => 'Spécifiez le montant (en @devise@) pour chaque détail de réservation',
 	'label_fieldset_specifier' => 'Spécifier',
 	'label_specifier_montant' => 'Spécifier le montant',
 
 	// M
-	'message_erreur_montant_credit' => 'Vous avez depassé la limite de votre crédit qui es de @credit@ !',	
+	'message_erreur_montant_credit' => 'Vous avez depassé la limite de votre crédit qui es de @credit@ !',
 	'message_erreur_montant_reservations_detail' => 'Le montant ne doit pas être supérieure à @montant_ouvert@ (montant encore à payer)',
 	'montant_paye' => 'Payé :',
 
-	
+
 	// P
+	'paiement_commande' => 'Paiement de la commande #@id_commande@',
 	'paiement_reservation' => 'Paiement de la réservation #@id_reservation@',
-	
 	// R
 	'reservation_bank_titre' => 'Réservations Bank',
 
